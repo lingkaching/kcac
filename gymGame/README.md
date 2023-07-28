a7d1624a770a1c5f9f31077d4d9ec8fd949fcb66
lingkaching <35302465+lingkaching@users.noreply.github.com>
2023-07-28 15:33:03 +0800

    Delete gymGame directory

--- gymGame/README.md
+++
@@ -1,9 +0,0 @@
-Unity game engine like framework to build OPENAI gym environments.
-
-To install,
-
-```bash
-git clone https://github.com/bhatiaabhinav/gymGame.git
-cd gymGame
-pip install -e .
-```
--- gymGame/gymGame/__init__.py
+++
@@ -1,3 +0,0 @@
-from gymGame.core import GameComponent, GameObject, Scene, set_execution_order
-from gymGame.colliders import Collider2D, BoxCollider2D
-from gymGame.rendering import Camera, SimpleSprite
